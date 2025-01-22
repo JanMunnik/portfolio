4a9eb529d25465274b9c684e94518660795f63d7
JanMunnik <143937964+JanMunnik@users.noreply.github.com>
2025-01-22 11:38:29 +0100

    added homepage first section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,26 @@
 
 <div class="h-screen">
 	<section class="flex items-center justify-evenly">
+		<img src="" alt="Ik die hard aan het werken is achter mijn laptop">
 		<h1 class="text-fluid-m text-yellow-300">JAN</h1>
-		<h1 class="text-fluid-m text-yellow-300">JAN</h1>
+		<a href="/#work">See my work</a>
+		<a href="/contact">Contact</a>
+		<!-- TODO: ik snap de link nog niet helemaal bij deze a (normaal doe ik ./ maar dit werkt nu niet) (wou het tijdelijk naar zelfde pagina linken)/ -->
 	</section>
-	<h1 class="text-fluid-m text-yellow-300">JAN</h1>
+	<section id="work">
+		<ul>
+			<li>
+				<img src="" alt="my UI work">
+				<a href="">UI</a>
+			</li>
+			<li>
+				<img src="" alt="my UX work">
+				<a href="">UX</a>
+			</li>
+			<li>
+				<img src="" alt="my graphic design work">
+				<a href="">Graphic design</a>
+			</li>
+		</ul>
+	</section>
 </div>
